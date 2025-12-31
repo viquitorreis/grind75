8a86e2932bf73a6819ed3dac22dfbd2131a02c87
victor <victorreisprog@gmail.com>
2025-12-31 18:41:20 -0300

    feat: 169

---
+++ 169-majority_element/1/main.go
@@ -0,0 +1,35 @@
+package main
+
+func main() {
+	nums := []int{2, 2, 1, 1, 1, 2, 2}
+	println(majorityElement(nums))
+}
+
+// solução interessante: https://leetcode.com/problems/majority-element/solutions/3220196/beats-9987-with-go-by-ratchawat-1rtu/
+
+// Complexity Analisys
+// BIG O - O(n) - linear time
+// 		loop on array only once, and most other operations (variables, conditionals, etc) are O(1) - constant
+// Time Complexity O(n) - linear
+//		Worst case scenario: all elements on array are different, so my hashmap will have one different key for each, so n entries on map
+//		Of course, asymptotically
+
+func majorityElement(nums []int) int {
+	seen := make(map[int]int, 0)
+	var (
+		biggestNum   = 0
+		biggestOccur = 0
+	)
+
+	for _, num := range nums {
+		seen[num]++
+		if count, ok := seen[num]; ok {
+			if biggestOccur < count {
+				biggestOccur = seen[num]
+				biggestNum = num
+			}
+		}
+	}
+
+	return biggestNum
+}
