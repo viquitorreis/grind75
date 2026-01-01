6e4ecc9c2f59f756e63a309edaf676db3657134f
victor <victorreisprog@gmail.com>
2026-01-01 10:30:06 -0300

    feat: 232 - 2

--- 232-implement-queue-using-stacks/2/main.go
+++ 232-implement-queue-using-stacks/2/main.go
@@ -17,14 +17,18 @@ func main() {
 	q.Pop()
 }
 
-/**
-* Your MyQueue object will be instantiated and called as such:
-* obj := Constructor();
-* obj.Push(x);
-* param_2 := obj.Pop();
-* param_3 := obj.Peek();
-* param_4 := obj.Empty();
- */
+// Complexity Analysis
+// BIG O
+//		- O(n)
+//			- Push: O(1) - in Go most of the times (unless underlying array needs to resize)
+//			- Pop: O(1) - the operation this.q = this.q[1:] creates a new SLICE HEADER pointing to the same underlying array
+// 				starting from index 1, that is constant time. However, there is a memory cost here, the old element isnt freed
+//				from the underlying array until the whole array is garbage collected, which can lead to memory waste over time
+//			- Peek: O(1)
+//			- Empty: O(1) - simple comparisons
+
+// SPACE COMPLEXITY
+//		- O(n) - where n is the elements currently stored in the queue
 
 type MyQueue struct {
 	q   []int
@@ -43,7 +47,7 @@ func (this *MyQueue) Push(x int) {
 	this.len++
 }
 
-// Removes the element from the front of the queue and returns it.
+// Pop -> Removes the element from the front of the queue and returns it.
 func (this *MyQueue) Pop() int {
 	if this.Empty() {
 		return -1
@@ -61,7 +65,7 @@ func (this *MyQueue) Pop() int {
 	return el
 }
 
-// returns the element at the front of the queue.
+// Peek -> returns the element at the front of the queue.
 func (this *MyQueue) Peek() int {
 	if this.Empty() {
 		return -1
